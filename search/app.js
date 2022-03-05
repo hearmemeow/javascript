40eb82f6d93145a28115f2e7812f2fa5f99c58b1
Samiksusamik <72317517+Samiksusamik@users.noreply.github.com>
2022-03-05 18:21:53 -0500

    Update app.js

--- search/app.js
+++ search/app.js
@@ -1,27 +1,71 @@
 const form = document.querySelector('#searchForm')
-const movies = document.querySelector('#movies')
-form.addEventListener('submit', async (e) => {
-    e.preventDefault()
-    console.log(form.elements.query.value)
+const movies1 = document.querySelector('#movies1')
+const movies2 = document.querySelector('#movies2')
+const movies3 = document.querySelector('#movies3')
+const movies4 = document.querySelector('#movies4')
+
+let contentAlreadyLoaded = false
+
+async function generateMovies(){
     let searchString = `https://api.tvmaze.com/search/shows?q=${form.elements.query.value}`
     let res = await axios.get(searchString)
     console.log(res.data)
+    let columnNumber = 0
+    contentAlreadyLoaded = true
     for(let result of res.data){
+        columnNumber++
+        let anchor = document.createElement('a')
         let container = document.createElement('div')
         let title = document.createElement('h3')
         let image = document.createElement('img')
-        movies.append(container)
-        container.append(title)
-        container.append(image)
-        title.innerText = result.show.name
-        try {
-        let imageString = result.show.image.medium
-        image.src = imageString
-        console.log(image.src)
-        } catch(e) {
-            console.log(e)
+        if(columnNumber === 1){
+            movies1.append(anchor)
+            anchor.append(container)
+            container.append(title)
+            container.append(image)
+        } else if(columnNumber === 2){
+            movies2.append(anchor)
+            anchor.append(container)
+            container.append(title)
+            container.append(image)
+        } else if(columnNumber === 3){
+            movies3.append(anchor)
+            anchor.append(container)
+            container.append(title)
+            container.append(image)
+        } else if(columnNumber === 4){
+            movies4.append(anchor)
+            anchor.append(container)
+            container.append(title)
+            container.append(image)
+        } else {
+            columnNumber = 1
+            movies1.append(anchor)
+            anchor.append(container)
+            container.append(title)
+            container.append(image)
         }
+        console.log(columnNumber)
+        title.innerText = result.show.name
+        image.src = result.show.image.medium
+        anchor.href = result.show.url
     }
+} 
+
+function deleteMovies(){
+    while (movies1.firstChild == true) {
+        movies1.removeChild(myNode.lastChild);
+    }
+}
+      
+
+form.addEventListener('submit', async (e) => {
+    // if(contentAlreadyLoaded === false){
+        e.preventDefault()
+        console.log(form.elements.query.value)
+        generateMovies()
+    // } else {
+    //     e.preventDefault()
+    // }
+    console.log('e')
 })
-
-
