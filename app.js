2af3b72cf6b2bf0e51e3e5e54ea390f3faf5ecdb
Samiksusamik <72317517+Samiksusamik@users.noreply.github.com>
2022-03-06 13:01:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,47 +4,29 @@ function copyToClipboardAsync(str) {
     return Promise.reject('The Clipboard API is not available.');
   };
 
-const copyable = document.querySelector('#copyable')
-
-copyable.addEventListener('click', () => {
-    copyToClipboardAsync(`<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css"></link>`)
-    copyable.style.color = 'green'
-    copyable.innerHTML = `<i class="fa-solid fa-copy"></i>
-    <span class="name" id="Bulma">Click To Copy Bulma and Axios Link</span>✓`
+  function copy(id, link ){
+    const copyable = document.querySelector(`#${id}`)
+    copyToClipboardAsync(link)
+    copyable.style.color = 'lightgreen'
     setTimeout(() => {
         copyable.style.color = 'black'
-        copyable.innerHTML = `<i class="fa-solid fa-copy"></i>
-        <span class="name" id="Bulma">Click To Copy Bulma and Axios Link</span>`
     }, 1500)
-})
-const allAnchors = document.querySelectorAll('.folder')
-const search = document.querySelector('#search')
-
-search.addEventListener('change', () => {
-    refreshSearch()
-})
-
-function refreshSearch() {
-    for(let a of allAnchors) {
-        let aText = a.innerText
-        let searchValue = search.value
-        if(aText.indexOf(searchValue) !== -1 && searchValue !== ''){
-            a.style.backgroundColor = 'yellow'
-        }
-        if(aText.indexOf(searchValue) == -1 || searchValue == '') {
-            a.style.backgroundColor = 'white'
-        }
-    }
 }
 
-const allCopys = document.querySelectorAll('.copy')
-const allProjects = document.querySelectorAll('.project')
-const allPanelBlocks = document.querySelectorAll('.panel-block')
+// Copy This Block For Copyable Text
+const bulmaAxios = document.querySelector('#bulmaAxios')
+bulmaAxios.addEventListener('click', () => {
+    copy('bulmaAxios', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>`)
+})
 
+// Tabs Functionality
 const allTab = document.querySelector('#all')
 const copyTab = document.querySelector('#copy')
 const projectTab = document.querySelector('#project')
+
+const allCopys = document.querySelectorAll('.copy')
+const allProjects = document.querySelectorAll('.project')
 
 allTab.addEventListener('click', () => {
     allTab.classList.add('is-active')
