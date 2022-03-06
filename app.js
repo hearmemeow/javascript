6e5512700b3e1be8aaf65bb2e5495e383bd1c026
Samiksusamik <72317517+Samiksusamik@users.noreply.github.com>
2022-03-06 13:02:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ function copyToClipboardAsync(str) {
       return navigator.clipboard.writeText(str);
     return Promise.reject('The Clipboard API is not available.');
   };
-
   function copy(id, link ){
     const copyable = document.querySelector(`#${id}`)
     copyToClipboardAsync(link)
@@ -12,22 +11,18 @@ function copyToClipboardAsync(str) {
         copyable.style.color = 'black'
     }, 1500)
 }
-
 // Copy This Block For Copyable Text
 const bulmaAxios = document.querySelector('#bulmaAxios')
 bulmaAxios.addEventListener('click', () => {
     copy('bulmaAxios', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>`)
 })
-
 // Tabs Functionality
 const allTab = document.querySelector('#all')
 const copyTab = document.querySelector('#copy')
 const projectTab = document.querySelector('#project')
-
 const allCopys = document.querySelectorAll('.copy')
 const allProjects = document.querySelectorAll('.project')
-
 allTab.addEventListener('click', () => {
     allTab.classList.add('is-active')
     copyTab.classList.remove('is-active')
@@ -39,7 +34,6 @@ allTab.addEventListener('click', () => {
         project.style.display = "block"
     }
 })
-
 copyTab.addEventListener('click', () => {
     allTab.classList.remove('is-active')
     copyTab.classList.add('is-active')
@@ -51,7 +45,6 @@ copyTab.addEventListener('click', () => {
         project.style.display = "none"
     }
 })
-
 projectTab.addEventListener('click', () => {
     allTab.classList.remove('is-active')
     copyTab.classList.remove('is-active')
